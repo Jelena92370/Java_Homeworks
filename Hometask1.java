f9ee6ef1b875953889039d6da82fc75f2d50c7b7
Jelena92370 <132999236+Jelena92370@users.noreply.github.com>
2023-06-22 21:30:14 +0200

    Homework1
    Uploaded

---
+++ Hometask1.java
@@ -0,0 +1,29 @@
+public class Hometask1 {
+    public static void main(String[] args) {
+        char newChar = 'G';
+        int newInt = 89;
+        byte newByte = 4;
+        short newShort = 56;
+        float newFloat = 4.7333436F;
+        double newDouble = 4.355453532;
+        long newLong = 12121;
+        boolean newBoolean = true;
+        String newStr = "New string";
+
+        System.out.println("Character: "+newChar);
+        System.out.println("Integer: "+newInt);
+        System.out.println("Byte: "+newByte);
+        System.out.println("Short: "+newShort);
+        System.out.println("Float: "+newFloat);
+        System.out.println("Double: "+newDouble);
+        System.out.println("Long: "+newLong);
+        System.out.println("Boolean: "+newBoolean);
+        System.out.println("Str: "+newStr);
+
+        //Задание 2
+        String task2 = "789";
+        System.out.println(task2.charAt(0) + ", " + task2.charAt(1) + ", " + task2.charAt(2));
+
+    }
+}
+
