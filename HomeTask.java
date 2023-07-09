6828c1d42c783e731835a9b2f1fe701c77d761d1
Jelena92370 <132999236+Jelena92370@users.noreply.github.com>
2023-07-09 20:35:15 +0200

    Hometask

--- HomeTask.java
+++ HomeTask.java
@@ -1,36 +1,37 @@
-package practice;
+package test.example;
 
 import java.util.Scanner;
 
-//Задание 1.
 public class HomeTask {
     public static void main(String[] args) {
+        int currentTime, start1, end1, start2, end2 = 0;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Старт работы магазина Edeka = ");
+        start1 = scanner.nextInt(24);
+        System.out.print("Окончание работы магазина Edeka = ");
+        end1 = scanner.nextInt(24);
+        System.out.print("\nСтарт работы магазина Rewe = ");
+        start2 = scanner.nextInt(24);
+        System.out.print("Окончание работы магазина Rewe = ");
+        end2 = scanner.nextInt(24);
 
-    Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите любое число");
-    double m = scanner.nextDouble();
-        System.out.println("Введите еще одно любое число");
-        double n = scanner.nextDouble();
+        System.out.println("\nВведите текущее время");
+        currentTime = scanner.nextInt(24);
 
-    int a = 10;
-        double b = (a - m);
-    if (b<0) b = (-b);
-        double c = (a - n);
-        if (c<0) c = (-c);
-        if(b==c)
-    {
-        System.out.println("Числа " + n + " и " + m + " одинаково близки к 10 ");
-    }
-        if(b>c)
 
-    {
-        System.out.println("Число " + n + " ближе к 10");
-    }
-        if(b<c)
+    boolean isEdekaOpen = workingShop(start1, end1, currentTime);
+    boolean isReweOpen = workingShop(start2, end2, currentTime);
 
-    {
-        System.out.println("Число " + m + " ближе к 10");
-
-    }
+    boolean canBuy = canBuy(isEdekaOpen, isReweOpen);
+        System.out.println("Можно ли купить еду? " + canBuy);
 }
+
+    static boolean workingShop(int start, int end, int currentTime) {
+        return currentTime >= start && currentTime <= end;
+    }
+
+    static boolean canBuy(boolean isEdekaOpen, boolean isReweOpen) {
+        return isEdekaOpen || isReweOpen;
+
+    }
 }
